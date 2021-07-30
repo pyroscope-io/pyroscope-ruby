044ff2e442661ed9484ba2b26b5081d0469532ae
Dmitry Filimonov <dmitry@pyroscope.io>
2021-07-30 20:56:20 +0000

    improvements

--- ext/pyroscope/extconf.rb
+++ ext/pyroscope/extconf.rb
@@ -9,7 +9,7 @@ HEADER_DIRS = [INCLUDEDIR]
 
 LIB_DIRS = [LIBDIR, File.expand_path(File.join(File.dirname(__FILE__), "lib"))]
 
-COMMIT = "39a3d9c"
+COMMIT = "a263249"
 
 # TODO: this is not very accurate, but it works for now
 OS = RUBY_PLATFORM.include?("darwin") ? "mac" : "linux"
@@ -25,32 +25,34 @@ if ENV["PYROSCOPE_RUBY_LOCAL"]
   system "cp #{ENV["HOME"]}/pyroscope/out/librustdeps.a #{File.join(ROOT, "lib/librustdeps.a")}"
 else
   Net::HTTP.start("dl.pyroscope.io", 443, :use_ssl => true) do |http|
-    req = Net::HTTP::Get.new(PREFIX+"/libpyroscope.rbspy.combo.a.gz")
-    http.request(req) do |resp|
-      raise "HTTP error: #{resp.code}" unless resp.code == "200"
+    ["libpyroscope.rbspy.a", "librustdeps.a"].each do |name|
+      req = Net::HTTP::Get.new(url)
+      http.request(req) do |resp|
+        raise "HTTP error: #{resp.code}" unless resp.code == "200"
 
-      r, w = IO.pipe
+        r, w = IO.pipe
 
-      t = Thread.new do
-        zreader = Zlib::GzipReader.new(r)
-        File.open(File.join(ROOT, "lib/libpyroscope.rbspy.combo.a"), "wb") do |f|
-          loop do
-            begin
-              chunk = zreader.readpartial(32768)
-              break if chunk.nil?
-            rescue EOFError
-              break
+        t = Thread.new do
+          zreader = Zlib::GzipReader.new(r)
+          File.open(File.join(ROOT, "lib/#{name}"), "wb") do |f|
+            loop do
+              begin
+                chunk = zreader.readpartial(32768)
+                break if chunk.nil?
+              rescue EOFError
+                break
+              end
+              f.write(chunk)
             end
-            f.write(chunk)
           end
         end
-      end
 
-      resp.read_body do |chunk|
-        w.write(chunk)
-      end
+        resp.read_body do |chunk|
+          w.write(chunk)
+        end
 
-      t.join
+        t.join
+      end
     end
   end
 end
@@ -60,7 +62,7 @@ end
 
 dir_config('pyroscope', HEADER_DIRS, LIB_DIRS)
 
-libs = ['-lpyroscope.rbspy.combo']
+libs = ['-lpyroscope.rbspy', '-lrustdeps']
 libs.each do |lib|
   $LOCAL_LIBS << "#{lib} "
 end
